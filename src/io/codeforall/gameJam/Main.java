a0285dd7dfff0f7649d2faf03ab510708360cbc8
RubenSantos <rubenfe2333@gmail.com>
2024-02-13 17:06:44 +0000

    Player class done

--- src/io/codeforall/gameJam/Main.java
+++ src/io/codeforall/gameJam/Main.java
@@ -1,13 +1,18 @@
 package io.codeforall.gameJam;
 
 
+import io.codeforall.gameJam.position.Players;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+
         Game game = new Game(35, 40);
         game.init();
-
+        Players player = new Players(new Picture(200,200,"resources/pawn.png"));
+        player.draw();
     }
 
 }
---
+++ src/io/codeforall/gameJam/position/Players.java
@@ -0,0 +1,30 @@
+package io.codeforall.gameJam.position;
+
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Players {
+    private Picture player;
+
+    public Players(Picture player) {
+        this.player = player;
+    }
+    public void draw(){
+        player.draw();
+    }
+
+    public void moveRight() {
+        player.translate(5, 0);
+    }
+
+    public void moveLeft() {
+        player.translate(-5, 0);
+    }
+
+    public void moveUp() {
+        player.translate(0,-5);
+    }
+
+    public void moveDown() {
+        player.translate(0,5);
+    }
+}
