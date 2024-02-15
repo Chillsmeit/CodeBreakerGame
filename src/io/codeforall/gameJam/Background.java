c74985765affe708909334308b79c6043829c031
RubenAlmeidaSantos <rubenfe2333@gmail.com>
2024-02-15 16:15:41 +0000

    Update Background.java

--- src/io/codeforall/gameJam/Background.java
+++ src/io/codeforall/gameJam/Background.java
@@ -2,7 +2,23 @@ package io.codeforall.gameJam;
 
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Background {
+public class Background implements Runnable {
+
+    public static int originalTileSize=32;
+
+    final static double scale= 1.0;
+
+    public static int PADDING=10;
+
+    public static int tileSize=(int)(originalTileSize*scale);
+
+    static public final int maxScreenCol=24;
+
+    static public final int maxScreenRow=18;
+
+    public static final int screenWidth=tileSize*(maxScreenCol);
+
+    public static final int screenHeight=tileSize*(maxScreenRow);
 
     private Picture background;
 
@@ -15,4 +31,8 @@ public class Background {
     }
 
 
+    @Override
+    public void run() {
+
+    }
 }
